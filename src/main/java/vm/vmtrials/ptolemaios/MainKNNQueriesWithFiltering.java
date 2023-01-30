57bdcd332437fc460b6036ece28e7628ef52e0f2
VladimirMic <xmic@fi.muni.cz>
2023-01-30 18:04:39 +0100

    Update MainKNNQueriesWithFiltering.java

--- src/main/java/vm/vmtrials/ptolemaios/MainKNNQueriesWithFiltering.java
+++ src/main/java/vm/vmtrials/ptolemaios/MainKNNQueriesWithFiltering.java
@@ -2,17 +2,22 @@ package vm.vmtrials.ptolemaios;
 
 import java.util.List;
 import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import vm.fs.dataset.FSDatasetInstanceSingularizator;
 import vm.fs.metricspace.distance.precomputedDistances.PrecomputedDistancesLoaderImpl;
 import vm.fs.store.queryResults.FSNearestNeighboursStorageImpl;
 import vm.fs.store.queryResults.FSQueryExecutionStatsStoreImpl;
+import vm.fs.store.queryResults.recallEvaluation.FSRecallOfCandidateSetsStorageImpl;
 import vm.metricspace.AbstractMetricSpace;
 import vm.metricspace.Dataset;
-import vm.metricspace.distance.bounding.twopivots.TwoPivotsFiltering;
-import vm.search.impl.KNNSearchWithTwoPivotFiltering;
 import vm.metricspace.distance.DistanceFunctionInterface;
 import vm.metricspace.distance.PrecomputedDistancesLoader;
+import vm.metricspace.distance.bounding.twopivots.TwoPivotsFiltering;
 import vm.metricspace.distance.bounding.twopivots.impl.PtolemaiosFiltering;
+import vm.queryResults.recallEvaluation.RecallOfCandsSetsEvaluator;
+import vm.search.SearchingAlgorithm;
+import vm.search.impl.KNNSearchWithTwoPivotFiltering;
 
 /**
  *
@@ -20,11 +25,22 @@ import vm.metricspace.distance.bounding.twopivots.impl.PtolemaiosFiltering;
  */
 public class MainKNNQueriesWithFiltering {
 
-    public static void main(String[] args) {
-        Dataset dataset = new FSDatasetInstanceSingularizator.RandomDataset20Uniform();
-//        String pathToHulls = "h:\\Skola\\2022\\Ptolemaions_limited\\EFgetBD\\Hulls\\" + dataset.getDatasetName() + "___tetrahedrons_100000__ratio_of_outliers_to_cut_0.01__pivot_pairs_128.csv";
-        int k = 100;
+    private static final Logger LOG = Logger.getLogger(MainKNNQueriesWithFiltering.class.getName());
 
+    public static void main(String[] args) {
+
+//        String pathToHulls = "h:\\Skola\\2022\\Ptolemaions_limited\\EFgetBD\\Hulls\\" + dataset.getDatasetName() + "___tetrahedrons_100000__ratio_of_outliers_to_cut_0.01__pivot_pairs_128.csv";
+        run(new FSDatasetInstanceSingularizator.SIFTdataset());
+        System.gc();
+        run(new FSDatasetInstanceSingularizator.MPEG7dataset());
+        System.gc();
+        run(new FSDatasetInstanceSingularizator.RandomDataset20Uniform());
+        System.gc();
+        run(new FSDatasetInstanceSingularizator.DeCAFDataset());
+    }
+
+    private static void run(Dataset dataset) {
+        int k = 100;
         AbstractMetricSpace metricSpace = dataset.getMetricSpace();
         DistanceFunctionInterface df = dataset.getDistanceFunction();
         int pivotCount = 512;
@@ -34,16 +50,27 @@ public class MainKNNQueriesWithFiltering {
         List pivots = dataset.getPivotsForTheSameDataset(pivotCount);
         float[][] pivotPivotDists = metricSpace.getDistanceMap(df, pivots, pivots);
 
-//        TwoPivotsFiltering filter = new PtolemaiosFilteringWithLimitedAngles(pathToHulls);
-        TwoPivotsFiltering filter = new PtolemaiosFiltering();
-        KNNSearchWithTwoPivotFiltering alg = new KNNSearchWithTwoPivotFiltering(metricSpace, filter, pivots, poDists, pd.getRowHeaders(), pd.getColumnHeaders(), pivotPivotDists, df);
+//        TwoPivotsFiltering filter = new PtolemaiosFilteringWithLimitedAngles(pivotCount + "_pivots", pathToHulls);
+        TwoPivotsFiltering filter = new PtolemaiosFiltering(pivotCount + "_pivots");
+//        TriangleInequality filter = new TriangleInequality(pivotCount + "_pivots");
+        SearchingAlgorithm alg = new KNNSearchWithTwoPivotFiltering(metricSpace, filter, pivots, poDists, pd.getRowHeaders(), pd.getColumnHeaders(), pivotPivotDists, df);
+//        SearchingAlgorithm alg = new KNNSearchWithOnePivotFiltering(metricSpace, filter, pivots, poDists, pd.getRowHeaders(), pd.getColumnHeaders(), df);
         TreeSet[] results = alg.completeKnnSearchOfQuerySet(metricSpace, queries, k, dataset.getMetricObjectsFromDataset());
-        FSQueryExecutionStatsStoreImpl statsStorage = new FSQueryExecutionStatsStoreImpl(dataset.getDatasetName(), dataset.getDatasetName(), k, dataset.getDatasetName(), dataset.getDatasetName(), filter.getNameOfBounds(), null);
+
+        LOG.log(Level.INFO, "Storing statistics of queries");
+        FSQueryExecutionStatsStoreImpl statsStorage = new FSQueryExecutionStatsStoreImpl(dataset.getDatasetName(), dataset.getDatasetName(), k, dataset.getDatasetName(), dataset.getDatasetName(), filter.getTechFullName(), null);
         statsStorage.storeStatsForQueries(alg.getDistCompsPerQueries(), alg.getTimesPerQueries());
         statsStorage.saveFile();
 
+        LOG.log(Level.INFO, "Storing results of queries");
         FSNearestNeighboursStorageImpl resultsStorage = new FSNearestNeighboursStorageImpl();
-        resultsStorage.storeQueryResults(queries, results, dataset.getDatasetName(), dataset.getDatasetName(), filter.getNameOfBounds());
+        resultsStorage.storeQueryResults(metricSpace, queries, results, dataset.getDatasetName(), dataset.getDatasetName(), filter.getTechFullName());
+
+        LOG.log(Level.INFO, "Evaluating accuracy of queries");
+        FSRecallOfCandidateSetsStorageImpl recallStorage = new FSRecallOfCandidateSetsStorageImpl(dataset.getDatasetName(), dataset.getDatasetName(), k, dataset.getDatasetName(), dataset.getDatasetName(), filter.getTechFullName(), null);
+        RecallOfCandsSetsEvaluator evaluator = new RecallOfCandsSetsEvaluator(new FSNearestNeighboursStorageImpl(), recallStorage);
+        evaluator.evaluateAndStoreRecallsOfQueries(dataset.getDatasetName(), dataset.getDatasetName(), k, dataset.getDatasetName(), dataset.getDatasetName(), filter.getTechFullName(), k);
+        recallStorage.saveFile();
     }
 
 }
