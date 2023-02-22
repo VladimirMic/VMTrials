943b477a636a40725d323d8f1c4fb9a0061309c6
VladimirMic <xmic@fi.muni.cz>
2023-02-22 01:14:52 +0100

    - ptolemy simple

--- src/main/java/vm/vmtrials/ptolemaios/LearnCAOverBDPlusEF.java
+++ src/main/java/vm/vmtrials/ptolemaios/LearnCAOverBDPlusEF.java
@@ -2,7 +2,7 @@ package vm.vmtrials.ptolemaios;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +15,7 @@ import vm.fs.store.auxiliaryForDistBounding.FSPtolemyInequalityWithLimitedAngles
 import vm.metricSpace.AbstractMetricSpace;
 import vm.metricSpace.Dataset;
 import vm.metricSpace.distance.DistanceFunctionInterface;
+import static vm.metricSpace.distance.bounding.twopivots.impl.PtolemaiosFilteringWithLimitedAnglesSimpleCoef.CONSTANT_FOR_PRECISION;
 import static vm.vmtrials.ptolemaios.PrintFirstStatsOfDataset.getData;
 
 /**
@@ -23,12 +24,12 @@ import static vm.vmtrials.ptolemaios.PrintFirstStatsOfDataset.getData;
  */
 public class LearnCAOverBDPlusEF {
 
+    public static final Float RATIO_OF_OUTLIERS_TO_CUT = 0.10f / 100f; // percentile defining the minimum and the maximum. I.e., 2 times this is ignored.
     public static final Integer NUMBER_OF_TETRAHEDRONS = 100000;
     public static final Integer SAMPLE_SET_SIZE = 100000;
-    public static final Integer PIVOT_PAIRS = 256;
-    public static final Integer CONSTANT_FOR_PRECISION = 10000;
+    public static final Integer PIVOT_PAIRS = 128;
 
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws IOException {
         Dataset dataset;
         dataset = new FSDatasetInstanceSingularizator.DeCAFDataset();
         run(dataset);
@@ -49,10 +50,8 @@ public class LearnCAOverBDPlusEF {
         List<Object> pivots = dataset.getPivotsForTheSameDataset(2 * PIVOT_PAIRS);
         Map<String, float[]> results = new HashMap<>();
         for (int p = 0; p < pivots.size(); p += 2) {
-            float minFractionSum = Float.MAX_VALUE;
-            float maxFractionSum = 0;
-            float minFractionDiff = Float.MAX_VALUE;
-            float maxFractionDiff = 0;
+            float[] fractionSums = new float[NUMBER_OF_TETRAHEDRONS];
+            float[] fractionDiffs = new float[NUMBER_OF_TETRAHEDRONS];
             Object[] fourObjects = new Object[4];
             fourObjects[0] = pivots.get(p);
             fourObjects[1] = pivots.get(p + 1);
@@ -71,17 +70,19 @@ public class LearnCAOverBDPlusEF {
                 float bd = Math.abs(sixDists[1] * sixDists[3]);
                 float fractionSum = CONSTANT_FOR_PRECISION * ac / (bd + ef);
                 float fractionDiff = ac / Math.abs(bd - ef);
-                minFractionSum = Math.min(minFractionSum, fractionSum);
-                maxFractionSum = Math.max(maxFractionSum, fractionSum);
-                minFractionDiff = Math.min(minFractionDiff, fractionDiff);
-                maxFractionDiff = Math.max(maxFractionDiff, fractionDiff);
+                fractionSums[i] = fractionSum;
+                fractionDiffs[i] = fractionDiff;
             }
+            Arrays.sort(fractionSums);
+            Arrays.sort(fractionDiffs);
+            int posMin = Math.round(NUMBER_OF_TETRAHEDRONS * RATIO_OF_OUTLIERS_TO_CUT);
+            int posMax = Math.round(NUMBER_OF_TETRAHEDRONS * (1 - RATIO_OF_OUTLIERS_TO_CUT)) - 1;
             String pivotPairsID = metricSpace.getIDOfMetricObject(fourObjects[0]) + "-" + metricSpace.getIDOfMetricObject(fourObjects[1]);
-            results.put(pivotPairsID, new float[]{minFractionSum, maxFractionSum, minFractionDiff, maxFractionDiff});
+            results.put(pivotPairsID, new float[]{fractionSums[posMin], fractionSums[posMax], fractionDiffs[posMin], fractionDiffs[posMax]});
             Logger.getLogger(LearnCAOverBDPlusEF.class.getName()).log(Level.INFO, "Evaluated coefs for pivot pair {0}", p / 2);
         }
         FSPtolemyInequalityWithLimitedAnglesCoefsStorageImpl storage = new FSPtolemyInequalityWithLimitedAnglesCoefsStorageImpl();
-        String resultDescription = storage.getResultDescription(dataset.getDatasetName(), NUMBER_OF_TETRAHEDRONS, PIVOT_PAIRS, 0);
+        String resultDescription = storage.getResultDescription(dataset.getDatasetName(), NUMBER_OF_TETRAHEDRONS, PIVOT_PAIRS, RATIO_OF_OUTLIERS_TO_CUT);
         storage.storeCoefficients(results, resultDescription);
     }
 }
