c399e6a45015fc60947fdb4c5b4e39f8fa2f56aa
xmic <xmic@fi.muni.cz>
2023-07-01 20:56:21 +0200

    Update Main.java

--- src/main/java/vm/vmtrials/tripleFiltering_Challenge/Main.java
+++ src/main/java/vm/vmtrials/tripleFiltering_Challenge/Main.java
@@ -142,7 +142,7 @@ public class Main {
     public static int getVoronoiK(int size) {
         switch (size) {
             case 100000:
-                return 20000;
+                return 40000;
             case 300000:
                 return 150000;
             case 10000000:
@@ -160,7 +160,7 @@ public class Main {
         switch (size) {
             case 100000:
             case 300000:
-                return 50;
+                return 100;
             default:
                 return 20000;
         }
