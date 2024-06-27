1a4cd46d6272efe93937943e0d26ac1191af5ad5
VladimirMic <xmic@fi.muni.cz>
2024-06-27 19:57:25 +0200

    Update PrintIDs.java

--- src/main/java/vm/vmtrials/checking/PrintIDs.java
+++ src/main/java/vm/vmtrials/checking/PrintIDs.java
@@ -5,7 +5,9 @@
 package vm.vmtrials.checking;
 
 import java.util.Iterator;
+import java.util.Map;
 import vm.fs.dataset.FSDatasetInstanceSingularizator;
+import vm.fs.metricSpaceImpl.FSMetricSpaceImpl;
 import vm.metricSpace.AbstractMetricSpace;
 import vm.metricSpace.Dataset;
 
@@ -16,14 +18,23 @@ import vm.metricSpace.Dataset;
 public class PrintIDs {
 
     public static void main(String[] args) {
-        Dataset dataset = new FSDatasetInstanceSingularizator.DeCAF100M_PCA256Dataset();
-        Iterator it = dataset.getMetricObjectsFromDataset();
+        Dataset dataset = new FSDatasetInstanceSingularizator.LAION_10M_PCA256Dataset();
+        run(dataset, true);
+    }
+
+    private static <T> void run(Dataset<T> dataset, boolean fromKeyValueStorage) {
+        Iterator it;
+        if (fromKeyValueStorage) {
+            it = dataset.getMetricObjectsFromDatasetKeyValueStorage();
+        } else {
+            it = dataset.getMetricObjectsFromDataset();
+        }
         AbstractMetricSpace metricSpace = dataset.getMetricSpace();
         Comparable oIdPrev = null;
-        for (int i = 0; i < 1000 && it.hasNext(); i++) {
+        for (int i = 0; it.hasNext(); i++) {
             Object o = it.next();
             Comparable id = metricSpace.getIDOfMetricObject(o);
-            System.out.print(id);
+            System.out.print(i + ": " + id);
             if (oIdPrev == null) {
                 System.out.println();
             } else {
