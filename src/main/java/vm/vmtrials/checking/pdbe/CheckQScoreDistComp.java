ee6b45e9d1d5f024c48513b242c90cc4ab059833
xmic <xmic@fi.muni.cz>
2024-10-29 20:44:15 +0100

    Working on PDBe df

---
+++ src/main/java/vm/vmtrials/checking/pdbe/CheckQScoreDistComp.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vm.vmtrials.checking.pdbe;
+
+import java.util.Iterator;
+import vm.fs.dataset.FSDatasetInstanceSingularizator;
+import vm.metricSpace.AbstractMetricSpace;
+import vm.metricSpace.Dataset;
+import vm.metricSpace.distance.DistanceFunctionInterface;
+
+/**
+ *
+ * @author xmic
+ */
+public class CheckQScoreDistComp {
+
+    public static void main(String[] args) {
+        Dataset dataset = new FSDatasetInstanceSingularizator.PDBePtoteinChainsDataset();
+        AbstractMetricSpace metricSpace = dataset.getMetricSpace();
+        Iterator it = dataset.getMetricObjectsFromDataset();
+        Object o1 = it.next();
+        Object o2 = it.next();
+        DistanceFunctionInterface df = dataset.getDistanceFunction();
+        float distance = df.getDistance(o1, o2);
+        System.out.print("The distance of " + metricSpace.getIDOfMetricObject(o1) + " and " + metricSpace.getIDOfMetricObject(o2) + " is " + distance);
+    }
+}
