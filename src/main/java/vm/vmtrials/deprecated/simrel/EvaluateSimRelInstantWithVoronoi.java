2bc35aa6c0342c35a1caaec6cb62c52e4a006f83
VladimirMic <xmic@fi.muni.cz>
2024-11-03 08:17:59 +0100

    Update EvaluateSimRelInstantWithVoronoi.java

--- src/main/java/vm/vmtrials/deprecated/simrel/EvaluateSimRelInstantWithVoronoi.java
+++ src/main/java/vm/vmtrials/deprecated/simrel/EvaluateSimRelInstantWithVoronoi.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package vm.vmtrials.hdf5;
+package vm.vmtrials.deprecated.simrel;
 
 import java.util.AbstractMap;
 import java.util.HashMap;
